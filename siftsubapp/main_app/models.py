91db99b23dbe3a1dc34567bcb32daf1ed5a911b6
latrisha <l_gomer@yahoo.com>
2025-05-01 14:09:18 -0700

    wip: add subscription model to models.py

--- siftsubapp/main_app/models.py
+++ siftsubapp/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subscription(models.Model):
+    name = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    renewal_date = models.DateField()
+    status = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
