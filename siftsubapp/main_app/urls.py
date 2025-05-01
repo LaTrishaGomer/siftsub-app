b1bc2ab28eecd5b969f1fc0a87d56440ade81a47
latrisha <l_gomer@yahoo.com>
2025-05-01 14:10:16 -0700

    wip: add URL patterns for home, about, signup, and subscription views

--- siftsubapp/main_app/urls.py
+++ siftsubapp/main_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
+    path('signup/', views.signup, name='signup'), 
+    path('subscriptions/', views.subscription_index, name='subscription-index'),
+    path('subscriptions/<int:subscription_id>/', views.subscription_detail, name='subscription-detail'),
+    path('subscriptions/create/', views.SubscriptionCreate.as_view(), name='subscription-create'),
 ]
+
 
