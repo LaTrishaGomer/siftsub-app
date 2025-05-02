b3db11173548a2296309625a27fdb73da8155c74
latrisha <l_gomer@yahoo.com>
2025-05-02 12:21:11 -0700

    wip: add signin route to urls.py

--- siftsubapp/main_app/urls.py
+++ siftsubapp/main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('signup/', views.signup, name='signup'), 
+    path('signin/', views.signin, name='signin'),
     path('subscriptions/', views.subscription_index, name='subscription-index'),
     path('subscriptions/<int:subscription_id>/', views.subscription_detail, name='subscription-detail'),
     path('subscriptions/create/', views.SubscriptionCreate.as_view(), name='subscription-create'),
